57835eee13d532741679daa6f5bc21957eaad910
altondillon <dillon.alton@gmail.com>
2017-07-27 15:55:51 -0500

    added settings

---
+++ fakechart.js
@@ -0,0 +1,44 @@
+var ctx = document.getElementById("myChart").getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ["Mac", "Windows", "Samsung Note 8", "Roku", "iPhone", "Alexa"],
+        datasets: [{
+            label: 'GB\'s Used',
+            data: [35, 59, 15, 25, 10, 63],
+            backgroundColor: [
+                'rgba(255, 50, 50, 0.7)',
+                'rgba(54, 162, 235, 0.5)',
+                'rgba(255, 206, 86, 0.5)',
+                'rgba(75, 192, 192, 0.5)',
+                'rgba(153, 102, 255, 0.5)',
+                'rgba(255, 159, 64, 0.5)'
+            ],
+            borderColor: [
+                'rgba(255,50,50,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        responsive: true,
+        maintainAspectRatio: true,
+        legend: {
+            display: true,
+            labels: {
+        }
+        },
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero:true
+                },
+            }]
+        }
+    }
+});
